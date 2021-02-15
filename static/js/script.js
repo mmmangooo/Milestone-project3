fae1c925cc067cabccfd21e899fb6684694587d2
mmmangooo <isabelle.angsas@gmail.com>
2021-02-15 09:45:33 +0000

    Styled contact form and updated code for upvote/downvote functionality

--- static/js/script.js
+++ static/js/script.js
@@ -12,38 +12,38 @@ $('.carousel').carousel({
 let counter = 0;
 $('#upVote').click(function(){
     counter++;
+    $('#count').text(counter);
+});
 $('#downVote').click(function(){
     counter--;
+    $('#count').text(counter);
 });
-$('#count').text(counter);
-});
+
+
 
 
 /* Contact form sending emails through emailjs on submission of form */
 
 function sendMail(contactForm) {
-    emailjs.send('default_service', 'template_pzyq6ap', {
-        'from_name': contactForm.name.value,
-        'from_email': contactForm.emailaddress.value,
+emailjs.send('default_service', 'template_pzyq6ap', {
+        'form_name': contactForm.name.value,
+        'form_email': contactForm.emailaddress.value,
         'message': contactForm.message.value
-    })
+})
 
-
-// Creates a response when the user sends the form, informing user if sending was successful or not 
-
-.then(
-
-    function(response){
-        console.log('success', response);
-        $('#success-alert').style.display='block';
-    },
-
-    function(error){
-        console.log('failed', error);
-        $('#error-alert').style.display='block';
+    // Creates a response when the user sends the form, informing user if sending was successful or not 
+    .then(
+        function(response) {
+            console.log('success', response);
+            $('#success-alert').style.display='block';
+    }, function(error) {
+            console.log('failed', error);
+            $('#error-alert').style.display='block';
     });
-
+  
+console.log('success')
 return false;
-}
+       
 
+}
 });
